28daec5db42504efb6396ef8a1b5fff395bc3ca0
Geo <geologz2064@gmail.com>
2025-01-06 18:27:21 +0100

    Ajout des menus du programme

--- Reaulight/mainwindow.cpp
+++ Reaulight/mainwindow.cpp
@@ -3,9 +3,16 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    setCentralWidget(new QWidget);
+    menuFichier = menuBar()->addMenu("&Fichier");
+    menuEdition = menuBar()->addMenu("&Edition");
+    menuAffichage = menuBar()->addMenu("&Affichage");
+
+
 }
 
 MainWindow::~MainWindow()
 {
 }
+
 
