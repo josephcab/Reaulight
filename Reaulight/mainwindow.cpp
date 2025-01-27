aea1d5fe15833befb4937423912337bd9945e3af
Geo <geologz2064@gmail.com>
2025-01-27 18:10:05 +0100

    Ajouts de plusieurs sous menus

--- Reaulight/mainwindow.cpp
+++ Reaulight/mainwindow.cpp
@@ -3,11 +3,25 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    this->window()->setGeometry(0,0,1000,600);
     setCentralWidget(new QWidget);
     menuFichier = menuBar()->addMenu("&Fichier");
+        QAction *actionOuvrir = new QAction("&Ouvrir", this);
+        menuFichier->addAction(actionOuvrir);
+        QMenu *fichiersRecents = menuFichier->addMenu("Fichiers &récents");
+        fichiersRecents->addAction("Fichier bidon 1.txt");
+        fichiersRecents->addAction("Fichier bidon 2.txt");
+        fichiersRecents->addAction("Fichier bidon 3.txt");
+        QAction *actionEnregistrer = new QAction("&Enregistrer", this);
+        menuFichier->addAction(actionEnregistrer);
+        QAction *actionEnregistrerSous = new QAction("&Enregistrer sous", this);
+        menuFichier->addAction(actionEnregistrerSous);
         QAction *actionQuitter = new QAction("&Quitter", this);
         menuFichier->addAction(actionQuitter);
+        connect(actionQuitter, SIGNAL(triggered()), qApp, SLOT(quit()));
+
     menuEdition = menuBar()->addMenu("&Edition");
+
     menuAffichage = menuBar()->addMenu("&Affichage");
 }
 
