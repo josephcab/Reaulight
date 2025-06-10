edc7dc71125ad1a56bf466b67a3d9a6b915a955c
Noe Poirier <noer5327@gmail.com>
2025-06-10 18:34:43 +0200

    Ajout de canaux gérant le mode son, stroboscope et le dimmer du projecteur courant ainsi qu'ajout de la plage de valeurs du canal courant

--- Reaulight/Items/channel.cpp
+++ Reaulight/Items/channel.cpp
@@ -1,8 +1,11 @@
 #include "channel.h"
 
-Channel::Channel(int place, QObject *parent) : QObject{parent}
+Channel::Channel(int place, QList<int> range_value, QObject *parent) : QObject{parent}
 {
     this->place = place;
+
+    if (range_value[0] <= range_value[1]) this->range_value = range_value;
+    else this->range_value = {0, 255};
 }
 
 void Channel::set_place(int place)
@@ -15,27 +18,48 @@ QString Channel::get_type() { return this->type; }
 
 int Channel::get_place() { return this->place; }
 
-void Channel::set_value(int value) { this->value = value; }
+void Channel::set_value(int value)
+{
+    if (value >= this->range_value[0] && value <= this->range_value[1])
+        this->value = value;
+}
 
 int Channel::get_value() { return this->value; }
 
-ChannelR::ChannelR(int place, QObject *parent) : Channel{place, parent}
+void Channel::set_range_value(QList<int> range)
 {
-    this->place = place;
-    this->value = 0;
+    if (range_value[0] <= range_value[1])
+        this->range_value = range;
+}
+
+QList<int> Channel::get_range_value() { return this->range_value; }
+
+ChannelDimmer::ChannelDimmer(int place, QList<int> range_value, QObject *parent) : Channel{place, range_value, parent}
+{
+    this->type = "Dimmmer channel";
+}
+
+ChannelStrobe::ChannelStrobe(int place, QList<int> range_value, QObject *parent) : Channel{place, range_value, parent}
+{
+    this->type = "Stroboscopic channel";
+}
+
+ChannelSound::ChannelSound(int place, QList<int> range_value, QObject *parent) : Channel{place, range_value, parent}
+{
+    this->type = "Sound Channel";
+}
+
+ChannelR::ChannelR(int place, QList<int> range_value, QObject *parent) : Channel{place, range_value, parent}
+{
     this->type = "Red channel";
 }
 
-ChannelG::ChannelG(int place, QObject *parent) : Channel{place, parent}
+ChannelG::ChannelG(int place, QList<int> range_value, QObject *parent) : Channel{place, range_value, parent}
 {
-    this->place = place;
-    this->value = 0;
     this->type = "Green channel";
 }
 
-ChannelB::ChannelB(int place, QObject *parent) : Channel{place, parent}
+ChannelB::ChannelB(int place, QList<int> range_value, QObject *parent) : Channel{place, range_value, parent}
 {
-    this->place = place;
-    this->value = 0;
     this->type = "Blue channel";
 }
